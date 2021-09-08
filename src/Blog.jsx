6c4d751c991be09aa49a38988d26cf0228c61ccf
SandeepSokle <60534028+SandeepSokle@users.noreply.github.com>
2021-09-08 22:22:46 +0530

    Update Blog.jsx

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,32 +2,38 @@
 
 
 import { useEffect, useState } from "react";
-import {Link} from "react-router-dom"
+import { Redirect, Link} from "react-router-dom"
 import "./Blog.css"
 import "./style.css"
 let Blog = (props)=>{
 
     let [newDataTitle , setNewDataTitle] = useState("");
     let [newDataBody , setNewDataBody] = useState("");
+ 
+    useEffect(() => {
+       if(props.user != null && props.allPost.length > 0){
 
-   useEffect(() => {
     let temp = props.allPost.filter((ele)=>{
         return (ele.id === props.user)
     })
-
+    
+    
     setNewDataTitle(temp[0].blogTitle)
     setNewDataBody(temp[0].blogBody)
     console.log(temp[0]);
+}
 }, [props.allPost,props.user])
 
-
-    return <div className = "blogDataContainer">
+    return (
+props.user === "" ? <Redirect to = "/"></Redirect> :
+    
+    <div className = "blogDataContainer">
 
         <div className = "blogTitle">{newDataTitle} </div>
 
         <div className = "blogBody">{newDataBody} </div>
         <button className = "blogBackBtn"><Link to = "/home">Back</Link></button>
-    </div>
+    </div>)
 }
 
 export default Blog;
